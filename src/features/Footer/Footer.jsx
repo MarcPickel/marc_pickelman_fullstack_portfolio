54fa8e59da06a170d46faaf6fdf2e506c4dc23e2
Marc Pickelman <marcpc@att.net>
2026-05-19 06:51:14 -0500

    Add/feature: add Footer with styles

--- src/features/Footer/Footer.jsx
+++ src/features/Footer/Footer.jsx
@@ -1,43 +1,48 @@
 import "./Footer.css";
 
+import { NavLink } from "react-router-dom";
+
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer__column">
-        <div className="footer__content">
-          <p className="footer__header">Marc Pickelman</p>
-          <p className="footer__text">Full-Stack Web Developer</p>
-          <p className="footer__text">Writing websites cover to cover</p>
-        </div>
-        <img className="footer__image" />
+        <h3 className="footer__header">Marc Pickelman</h3>
+        <p className="footer__text">Full-Stack Web Developer</p>
+        <p className="footer__text">Writing websites from cover to cover</p>
       </div>
       <div className="footer__column">
         <p className="footer__column-header">Pages</p>
-        <a href="" className="">
-          Home
-        </a>
-        <a href="" className="">
-          SEO
-        </a>
-        <a href="" className="">
-          Writing
-        </a>
-        <a href="" className="">
-          About
-        </a>
+        <NavLink to="/">
+          <p className="footer__link">Home</p>
+        </NavLink>
+        <NavLink to="/seo">
+          <p className="footer__link">SEO</p>
+        </NavLink>
+        <NavLink to="/writing">
+          <p className="footer__link">Writing</p>
+        </NavLink>
+        <NavLink to="">
+          <p className="footer__link">Moria</p>
+        </NavLink>
+        <NavLink to="/about">
+          <p className="footer__link">About</p>
+        </NavLink>
       </div>
       <div className="footer__column">
         <p className="footer__column-header">Get ahold of me</p>
-        <a href="" className="">
-          Phone
-        </a>
-        <a href="" className="">
+        <a href="mailto:marcpc@att.net" className="footer__link">
           Email
         </a>
-        <a href="" className="">
+        <a href="tel:+19063228501" className="footer__link">
+          Phone
+        </a>
+        <a
+          href="https://www.linkedin.com/in/marc-pickelman"
+          className="footer__link"
+        >
           LinkedIn
         </a>
-        <a href="" className="">
+        <a href="https://github.com/MarcPickel" className="footer__link">
           GitHub
         </a>
       </div>
